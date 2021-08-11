65661e0ab67df070538c3bbf46eb37736273eefb
Adnan20201 <afifuladnan2020@gmail.com>
2021-08-12 04:05:08 +0600

    Add body

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,55 +1,15 @@
-(function ($) {
+$(function () {
     'use strict';
 
-    var imJs = {
-        m: function (e) {
-            imJs.d();
-            imJs.methods();
-        },
-        d: function (e) {
-            this._window = $(window),
-            this._document = $(document),
-            this._body = $('body'),
-            this._html = $('html')
-
-        },
-
-        methods: function (e) {
-            imJs.featherAtcivation();
-            imJs.backToTopInit();
-            imJs.mobileMenuActive();
-            imJs.vedioActivation();
-            imJs.stickyHeader();
-            imJs.smothScroll();
-            imJs.smothScroll_Two();
-            imJs.stickyAdjust();
-            imJs.testimonialActivation();
-            imJs.contactForm();
-            imJs.wowActive();
-            imJs.awsActivation();
-            imJs.demoActive();
-            imJs.activePopupDemo();
-            
-        },
-
-        
-        activePopupDemo: function (e) {
-            $('.popuptab-area li a.demo-dark').on('click', function (e) {
-                $('.demo-modal-area').addClass('dark-version');
-                $('.demo-modal-area').removeClass('white-version');
-            });
-
-            $('.popuptab-area li a.demo-light').on('click', function (e) {
-                $('.demo-modal-area').removeClass('dark-version');
-                $('.demo-modal-area').addClass('white-version');
-            })
-        },
-
-        demoActive: function (e) {
-            $('.rn-right-demo').on('click', function (e) {
-                $('.demo-modal-area').addClass('open');
-            })
-            $('.demo-close-btn').on('click', function (e) {
-                $('.demo-modal-area').removeClass('open');
-            })
-        },
+        $('.testimonial-activation').slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+            arrows: true,
+            adaptiveHeight: true,
+            cssEase: 'linear',
+            prevArrow: '<button class="slide-arrow prev-arrow"><i class="fas fa-arrow-left"></i></button>',
+            nextArrow: '<button class="slide-arrow next-arrow"><i class="fas fa-arrow-right"></i></button>'
+        });
+})
