9b9f2c4be5e4a8bd536d64cd5f9adfe056d4b687
Adnan20201 <afifuladnan2020@gmail.com>
2021-08-16 00:36:54 +0600

    Add body

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,4 +12,63 @@ $(function () {
             prevArrow: '<button class="slide-arrow prev-arrow"><i class="fas fa-arrow-left"></i></button>',
             nextArrow: '<button class="slide-arrow next-arrow"><i class="fas fa-arrow-right"></i></button>'
         });
+
+        $('.testimonial-item-one').slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+            arrows: true,
+            adaptiveHeight: true,
+            cssEase: 'linear',
+            prevArrow: '<button class="slide-arrow prev-arrow"><i class="fas fa-arrow-left"></i></button>',
+            nextArrow: '<button class="slide-arrow next-arrow"><i class="fas fa-arrow-right"></button>',
+            responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    arrows: false,
+                }
+            }]
+        });
+
+       
+        ('.portfolio-slick-activation').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: true,
+            cssEase: 'linear',
+            adaptiveHeight: true,
+            prevArrow: '<button class="slide-arrow prev-arrow"><i class="feather-arrow-left"></i></button>',
+            nextArrow: '<button class="slide-arrow next-arrow"><i class="feather-arrow-right"></i></button>',
+            responsive: [{
+                    breakpoint: 1124,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 868,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 576,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        dots: true,
+                        arrows: false,
+                    }
+                }
+            ]
+        });
+
+
+
 })
